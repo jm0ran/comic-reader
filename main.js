5c4913f353212d77910e8c00d94edfbd5abac80f
jm0ran <josephdeargmoran@gmail.com>
2021-03-08 21:09:02 -0500

    Returns Comic Folders and Comic Contents

--- main.js
+++ main.js
@@ -16,34 +16,30 @@ const comic1 = path.join(__dirname, "public", "comics", "1/");
 
 
 app.get("/api/comics", (req, res) => { //When going to a website your computer is performing a get request, / is root of website, followed by function with request and response
-    var returnData = new Array;
     var comicFolders = new Array;
     fs.readdir(path.join(__dirname, "public", "comics"), (err, files) =>{ //Read all files in /public/comics 
         if (err) throw err; //If error throw error
         files.forEach((file) => { //For each file
             if (fs.lstatSync(path.join(__dirname, "public", "comics", file)).isDirectory()){ //If the given file is a directory
-                comicFolders.push(file)
+                comicFolders.push(file)//Push folder name to comicFolders Array
             }
         })
-        for (const comic of comicFolders){
-            returnData.push(new Comic(comic))
-        }
-
-        res.json(returnData);
+        var returnData = new Array; //Create returnData array
+        for (const comic of comicFolders){ //For 
+            let comicPath = path.join(__dirname, "public", "comics", comic)
+            fs.readdir(comicPath, (err, images) =>{
+                if (err) throw err;
+                let itComic = new Comic(comic);
+                images.forEach((image) => {
+                    itComic.images.push(image);
+                })
+                returnData.push(itComic);
+                if (comic == comicFolders[comicFolders.length - 1]){
+                    res.json(returnData);
+                }
+            })
+        }        
     })
-    
-    
-    
-    /*
-    fs.readdir(comic1, (err, files) =>{
-        if (err) throw err;
-        console.log(files);
-        (files.forEach((file) => {
-            console.log(file);
-        })
-        res.json(files);
-    })
-    */
 });
 
 
