0a692fa25ee9977407f7ce4760a06e7f02b6ada1
jm0ran <josephdeargmoran@gmail.com>
2021-03-17 22:38:09 -0400

    Starting Promise Implementation

--- grabThumbs.js
+++ grabThumbs.js
@@ -1,42 +1,44 @@
 fs = require("fs");
 path = require("path");
 jimp = require("jimp");
+const { dir } = require("console");
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
+/* const compressPNG = async (oPath, newName) => {
+    console.log(oPath);
+    const dest = path.join(__dirname, "public", "assets", "thumbnails", newName + ".jpg");
+    const file = await imagemin([oPath], {
+        destination: dest,
+        plugins: [
+            imageminMozjpeg({
+                quality: 30
+            })
+        ]
+    })
+    console.log(file);
+    //console.log(`Compressed to ${dest} from ${oPath}`)
+} */
+
+
 
 module.exports = {
     async run(){
-        fs.readdir(path.join(__dirname, "public", "comics"), (err, folders) =>{ //Read all files in /public/comics 
-            if (err) throw err; //If error throw error
-            let results = [];
-            folders.forEach((folder, index) => { //For each file
-                if (fs.lstatSync(path.join(__dirname, "public", "comics", folder)).isDirectory()){ //If the given file is a directory
-                    fs.readdir(path.join(__dirname, "public", "comics", folder), (err, files) =>{
-                      files.forEach((cover, index)=>{
-                          if (index === 0){
-                            let extension = ".jpg";
-                            source = path.join(__dirname, "public", "comics", folder, cover);
-                            destination = path.join(__dirname, "public", "assets", "testCompression", folder + extension);
-                            if (cover.endsWith(".png")){
-                               jimp.read(source, (err, image) => {
-                                   if (err){
-                                       throw err;
-                                   }else{
-                                       image.write(destination);
-                                   }
-                               }) 
-                            }
-                            else{
-                                fs.copyFile(source, destination, (err) =>{
-                                    if (err) throw err;
-                                })
-                            }
-                          }
-                          else{
-                              return false;
-                          }
-                      })  
-                    })
+        fs.promises.readdir(path.join(__dirname, "public", "comics"))
+        
+        .then(returnedFiles => {
+            let directories = returnedFiles.filter(file => {
+                let isDirectory = false;
+                if (fs.lstatSync(path.join(__dirname, "public", "comics", file)).isDirectory()){
+                    isDirectory = true;
                 }
+                return isDirectory;
             })
+            return(directories);
         })
+        .then(directories => {
+            directories.forEach(folder => console.log(folder));
+        })
+        .catch(err => console.log(err.message));
     }
 }
--- main.js
+++ main.js
@@ -3,10 +3,9 @@ const path = require("path"); //Node js module to deal with file paths
 const fs = require("fs"); //fs is used to work with the file system
 const ejs = require("ejs")//
 
-const generateThumbs = require("./generateThumbs");
 const grabThumbs = require("./grabThumbs");
 
-generateThumbs.run();
+grabThumbs.run();
 
 const app = express(); //Initializes a new app variable with express
 
